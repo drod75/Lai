5cff37125b8a5600929d1916ed3fdcb12b5b94b0
David Rodriguez <dr507498@gmail.com>
2026-02-06 18:29:36 -0500

    test 1 formatted

--- testing/audio_test_1.py
+++ testing/audio_test_1.py
@@ -3,23 +3,31 @@ import os
 from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
 
+
 @st.cache_resource
 def load_env():
     load_dotenv()
-    
+
+
 @st.cache_resource
 def load_elevenlabs():
     elevenlabs = ElevenLabs(
         api_key=os.getenv("ELEVENLABS_API_KEY"),
     )
-    
-    if 'elevenlabs' not in st.session_state:
-        st.session_state['elevenlabs'] = elevenlabs
+
+    if "elevenlabs" not in st.session_state:
+        st.session_state["elevenlabs"] = elevenlabs
+
 
 load_env()
 load_elevenlabs()
 
-st.set_page_config(page_title="Lai Test 1", page_icon="⚠️", layout="wide",initial_sidebar_state="expanded")
+st.set_page_config(
+    page_title="Lai Test 1",
+    page_icon="⚠️",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 st.header("Audio Test 1")
 
@@ -28,7 +36,7 @@ st.audio(audio_value)
 
 with st.expander("Speech to Text Test!"):
     if audio_value:
-        transcription = st.session_state['elevenlabs'].speech_to_text.convert(
+        transcription = st.session_state["elevenlabs"].speech_to_text.convert(
             file=audio_value,
             model_id="scribe_v2",
             tag_audio_events=True,
@@ -37,6 +45,6 @@ with st.expander("Speech to Text Test!"):
         )
 
         st.markdown(transcription.text)
-        
+
     else:
         st.warning("No audio yet! :(")
